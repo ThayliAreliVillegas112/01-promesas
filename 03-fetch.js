a694f32c5338f9114fb60d937440a87f7de3132d
ThayliAreliVillegas112 <20203tn064@utez.edu.mx>
2023-05-10 00:32:43 -0600

    modal

--- 03-fetch.js
+++ 03-fetch.js
@@ -22,14 +22,12 @@ resp.then(response=> response.json())
 
         let card = `<div class="col-4"> <div class="card">
             
-            <div class="card-header">
-                 API
-            </div>
+          
             <div class="card-body">
                 <h5>Email:${element.email}</h5>
                 <h5>First name:${element.first_name}</h5>
                 <h5>Last name:${element.last_name}</h5>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <button type="button" data-bs-toggle="modal" data-bs-target="#details" class="btn btn-info center" onclick="findById(${element.id})">Detalles</button>
             </div>
           </div>
         </div>`
@@ -41,36 +39,14 @@ content.innerHTML=htmlX;
 
 
 
-const resp2 = fetch('https://reqres.in/api/users?page=1');
-resp.then(response=> response.json())
-.then((json)=>{
-    
-    let content = document.getElementById('modal');
-    let htmlX= '';
-
-    json.data.id.forEach(element => {
-        console.log("element")
-        console.log(element);
-
-        let card = `<div class="modal" tabindex="-1">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title">Modal title</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              <p>Modal body text goes here.</p>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-          </div>
-        </div>
-      </div>`
-        htmlX= htmlX+card;
-});
-content.innerHTML=htmlX;
-    
-})
+const findById = (id) =>{
+  fetch('https://reqres.in/api/users/'+id)
+  .then(response => response.json())
+  .then(json => {
+      console.log(json.data)
+      document.getElementById("name").innerHTML = json.data.first_name;
+      document.getElementById("last").innerHTML = json.data.last_name;
+      document.getElementById("email").innerHTML = json.data.email
+      document.getElementById("avatar").src = json.data.avatar
+  })
+}
